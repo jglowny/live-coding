67bca58dc5b9e8c21a3641bcfd1982e2a91eec93
Jakub G <jakub.glowny@gmail.com>
2024-07-25 14:13:04 +0200

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable eqeqeq */
+import { useState, useEffect } from 'react';
+import './App.scss';
+
+type People = {
+  name: string;
+  height: number;
+  skin_color: string;
+  onClick: () => void;
+}
 
 function App() {
+
+  const [ data, setData] = useState<People[]>([])
+  const [selectedItem, setSelectedItem] = useState(null)
+
+  const handleClick = (item:any) => {
+    console.log(item);
+    if(item !== selectedItem){
+      setSelectedItem(item)
+    }
+    else{
+      setSelectedItem(null)
+    }
+    }
+
+  useEffect(() => {
+    fetch("https://swapi.dev/api/people/")
+    .then((response => {
+      if(response.ok){
+        return response.json();
+      }
+      throw new Error("invalid response");
+    }))
+    .then((data) => setData(data.results));
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Skin_color</th>
+            <th>Details</th>
+          </tr>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td >
+                <span 
+                 style={{ color: index === selectedItem  ? 'red' : 'black' }}
+                 onClick={() => handleClick(index)}
+          >{item.name}</span>
+              </td>
+              <td>
+                {item.height}
+              </td>
+              <td>
+                {item.skin_color}
+              </td>
+              <td></td>
+              </tr>
+            ))}
+            
+        </table>
     </div>
   );
 }
